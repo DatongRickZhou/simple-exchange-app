6ce639a030596ba0695237e4feadca9f598da770
DatongRickZhou <37567778+DatongRickZhou@users.noreply.github.com>
2019-05-16 16:09:19 +1000

    implement fireAuth plugin

---
+++ src/app/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afAuth:AngularFireAuth) { }
+  signUp(email:string,password:string){
+    return new Promise// 发送，直到接收到返回值，继续，只返回一次
+    ((resolve,reject)=>{
+      this.afAuth.auth.createUserWithEmailAndPassword(email,password)
+      .then((response)=>{
+        resolve(response)})
+      .catch((error)=>{reject(error)})
+    });
+    }
+    signOut(){
+      return new Promise((resolve,reject)=>{
+        this.afAuth.auth.signOut()
+        .then(()=>{resolve(true)})
+        .catch((error)=>{reject(error)});
+      });
+    }
+    signIn(email:string,password:string){
+      return new Promise((resolve,reject)=>{
+        this.afAuth.auth.signInWithEmailAndPassword(email,password)
+        .then((response)=>{
+          resolve(response)})
+        .catch((error)=>{reject(error)})
+      });
+    }
+}
